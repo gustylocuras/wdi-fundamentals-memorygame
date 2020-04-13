d010b85e2bfe77b84a005af5e243d6ddf59afe0b
Agustin <alvarez.escalante.m@gmail.com>
2020-04-13 13:51:50 -0700

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,20 +4,32 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
-cardsInPlay.push(cardOne);
+function flipCard(cardId){
 
-console.log("User flipped " + cardOne);
+console.log("User flipped " + cards[cardId]);
 
-let cardTwo = cards[1];
+cardsInPlay.push(cards[cardId]);
 
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
 
 if(cardsInPlay.length === 2){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!")
 	} else { alert("Sorry, try again");}
+
+checkForMatch();
+
 }
+}
+
+
+flipCard(0);
+flipCard(2);
+
